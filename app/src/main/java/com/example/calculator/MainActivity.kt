fbdb248027c7500f8ef36968121e98c470a52b6f
thedarkalchemist16 <shivangj.1601@gmail.com>
2022-05-29 10:20:51 +0530

    FIFTH Commit

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -6,10 +6,16 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
 
     private var tvInput: TextView? = null
+    private var LastNumeric:Boolean=false
+    private var LastDot:Boolean=false
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,9 +25,128 @@ class MainActivity : AppCompatActivity() {
     fun OnDigit(view: View){
 
         tvInput?.append((view as Button).text)
+        LastNumeric=true
+        LastDot=false
+
     }
 
     fun OnClear(view: View){
         tvInput?.text=""
+    }
+
+    fun OnDecimalPoint(view: View){
+        if(LastNumeric&&!LastDot){
+            tvInput?.append(".")
+            LastNumeric=false
+            LastDot=true
+        }
+
+    }
+
+    fun OnOperator(view: View){
+        tvInput?.text?.let {
+            if(LastNumeric && !IsOperatorAdded(it.toString())){
+                tvInput?.append((view as Button).text)
+                LastNumeric=false
+                LastDot=false
+
+            }
+        }
+
+    }
+
+    fun OnEqual(view: View){
+        if(LastNumeric){
+            var tvValue=tvInput?.text.toString()
+            var prefix=""
+
+            try{
+                if(tvValue.startsWith("-")){
+                    prefix="-"
+                    tvValue=tvValue.substring(1)
+                }
+
+                if(tvValue.contains("-")){
+
+                    val SplitValue=tvValue.split("-")
+                    var one=SplitValue[0]
+                    var two=SplitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one=prefix+one
+                    }
+
+                    tvInput?.text=(one.toDouble()-two.toDouble()).toString()
+                }
+
+                else if(tvValue.startsWith("+")){
+                    prefix="+"
+                    tvValue=tvValue.substring(1)
+                }
+
+                if(tvValue.contains("+")){
+
+                    val SplitValue=tvValue.split("+")
+                    var one=SplitValue[0]
+                    var two=SplitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one=prefix+one
+                    }
+
+                    tvInput?.text=(one.toDouble()+two.toDouble()).toString()
+                }
+
+                else if (tvValue.startsWith("*")){
+                    prefix="*"
+                    tvValue=tvValue.substring(1)
+                }
+
+                if(tvValue.contains("*")){
+
+                    val SplitValue=tvValue.split("*")
+                    var one=SplitValue[0]
+                    var two=SplitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one=prefix+one
+                    }
+
+                    tvInput?.text=(one.toDouble()*two.toDouble()).toString()
+                }
+
+                else if (tvValue.startsWith("/")){
+                    prefix="/"
+                    tvValue=tvValue.substring(1)
+                }
+
+                if(tvValue.contains("/")){
+
+                    val SplitValue=tvValue.split("/")
+                    var one=SplitValue[0]
+                    var two=SplitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one=prefix+one
+                    }
+
+                    tvInput?.text=(one.toDouble()-two.toDouble()).toString()
+                }
+
+
+        }catch(e: ArithmeticException){
+            e.printStackTrace()
+
+            }        }
+    }
+
+    private fun IsOperatorAdded (value:String):Boolean{
+        return if(value.startsWith("-")){
+            false
+        }
+        else(value.contains("+")
+                || value.contains("*")
+                || value.contains("/")
+                || value.contains("-"))
     }
 }
